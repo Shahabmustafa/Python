cf22f2b96c17e1dd4286b9dc2910eadeec3a1f25
Shahab Mustafa <shahabmustafa57@gmail.com>
2023-07-18 22:24:34 +0500

    discuse string

---
+++ great learning/string.py
@@ -0,0 +1,11 @@
+name = "Shahab,Mustafa"
+
+print(name[0 : 6])
+print(name[7 : 14])
+print(name.lower())
+print(name.__len__())
+print(name.upper())
+print(name.replace('Shahab','Kashif '))
+print(name.count('M'))
+print(name.find('b'))
+print(name.split(','))
--- great learning/varibale.py
+++ great learning/varibale.py
@@ -1,8 +1,34 @@
 a = 10
 b = 20
-
+# Arithmetic Operater
 add = a + b
 mult = a * b
 sub = a - b
 div = a / b 
 print(div)
+
+employe_name = "Shahab Mustafa"
+
+print(type(employe_name))
+
+# Relational Operator
+# <,=,!,>, ==
+
+# print(a>b)
+# print(a<b)
+# print(a == b)
+# print(a != b)
+
+# Logic Operator
+# & and,| or
+
+a = False
+b = True
+
+print(a|b)
+print(b|a)
+print(a|a)
+print(b|b)
+
+print(a&b)
+print(b&a)
