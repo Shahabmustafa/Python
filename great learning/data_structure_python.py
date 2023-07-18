33a49bd93855118599a1282b2e8ed6cdcffd43ae
Shahab Mustafa <shahabmustafa57@gmail.com>
2023-07-18 22:55:27 +0500

    discuse tuple and tuple function

---
+++ great learning/data_structure_python.py
@@ -0,0 +1,15 @@
+
+# this is called tuple 
+tuple = (1,'a',True,2,'b',False)
+print(tuple)
+print(type(tuple))
+print(tuple[0])
+print(tuple[-1])
+print(tuple[1:3])
+print(len(tuple))
+
+tuple1 = (1,2,3)
+tuple2 = (4,5,6)
+
+print(tuple1 + tuple2)
+print(tuple1*2 + tuple2)
